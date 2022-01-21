0f3473cd7bbd7a7924c480dac0377aa802fc1b74
mh <hyunjin@oddconcepts.kr>
2022-01-21 13:00:19 +0900

    add url info

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,15 @@
 const API_KEY = "3aad675abb5a1b5b1d049a1ce174debe";
 const weatherIcons = ['Clear', 'Cloud', 'Rain', 'Snow'];
 
+const url = window.location.href;
+const alertArea = document.querySelector('.alert');
+const alertLine = document.createElement('p');
+const alertMessage = "안내: https로 접속해주세요.";
+if(url.includes('http://')){
+    alertArea.appendChild(alertLine);
+    alertLine.innerText = alertMessage;
+}
+
 function onGeoOk(position) {
     const lat = position.coords.latitude ? position.coords.latitude : 37.583;
     const lon = position.coords.longitude ? position.coords.longitude : 127.0544;
