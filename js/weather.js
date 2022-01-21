599cbc9678f5a828fbd09e9dc368a8ac050e301e
mh <hyunjin@oddconcepts.kr>
2022-01-21 10:03:14 +0900

    add default position

--- js/weather.js
+++ js/weather.js
@@ -2,8 +2,8 @@ const API_KEY = "3aad675abb5a1b5b1d049a1ce174debe";
 const weatherIcons = ['Clear', 'Cloud', 'Rain', 'Snow'];
 
 function onGeoOk(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+    const lat = position.coords.latitude ? position.coords.latitude : 37.583;
+    const lon = position.coords.longitude ? position.coords.longitude : 127.0544;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector('#weather .condition span');
